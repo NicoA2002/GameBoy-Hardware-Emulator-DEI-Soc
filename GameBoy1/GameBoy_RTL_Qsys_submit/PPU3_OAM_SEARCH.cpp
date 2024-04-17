372b3b66d071477670e449a607ef4bfdae688c7d
Donovan Sproule <donovan.sproule@yahoo.com>
2024-04-17 11:52:07 -0400

    Prototype OAM search test

--- GameBoy1/GameBoy_RTL_Qsys_submit/PPU3_OAM_SEARCH.cpp
+++ GameBoy1/GameBoy_RTL_Qsys_submit/PPU3_OAM_SEARCH.cpp
@@ -35,9 +35,33 @@ int main(int argc, const char ** argv, const char ** env) {
 	for (i = 0; i < 5; i++)
 		ADD_ENT(offset * i);
 
-	// Verilated::commandArgs(argc, argv);
+	Verilated::commandArgs(argc, argv);
 
-	// VPPU3 *dut = new VPPU3;  // Instantiate the collatz module
+	VPPU3 *dut = new VPPU3;  // Instantiate the ppu module
+
+	Verilated::traceEverOn(true);
+	VerilatedVcdC *tfp = new VerilatedVcdC;
+
+	dut->trace(tfp, 99);
+	tfp->open("ppu3.vcd");
+
+	// output logic PPU_RD,
+    // output logic [15:0] PPU_ADDR,
+    // input logic [7:0] PPU_DATA_in,
+
+	for (time = 0 ; time < 10000 ; time += 10) {
+    	dut->clk = ((time % 20) >= 10) ? 1 : 0; // Simulate a 50 MHz clock
+
+    	dut->PPU_DATA_in = OAM_BUFF[dut->PPU_ADDR - 0xFE00];
+    	dut->eval();     // Run the simulation for a cycle
+    	tfp->dump(time); // Write the VCD file for this cycle
+    }
+
+	tfp->close(); // Stop dumping the VCD file
+	delete tfp;
+
+	dut->final(); // Stop the simulation
+	delete dut;
 
 	return exit_code;
 }
