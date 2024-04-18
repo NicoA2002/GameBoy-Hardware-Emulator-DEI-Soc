2074378de8fdab9337074caa147b9b8883cae49f
NicoA2002 <114366568+NicoA2002@users.noreply.github.com>
2024-04-18 00:14:30 -0400

    draw bg map tb

--- Testbench/tb_ppu.cpp
+++ Testbench/tb_ppu.cpp
@@ -3,40 +3,27 @@
 #include <verilated.h>
 #include <verilated_vcd_c.h>
 
-typedef struct {
-	char y;
-	char x;
-	char tile_no;
-	char flags;
+// input logic [15:0] ADDR,
+// input logic WR,
+// input logic [7:0] MMIO_DATA_out,
+// output logic [7:0] MMIO_DATA_in,
 
-} OAM_Ent;
+
 
 int main(int argc, const char ** argv, const char ** env) {
-	char OAM_BUFF[160];
+	char Tile_Map[1024];
+
+	for (int i = 0; i<8; i++) Tile_Map[240 + i] = 1;
+
 	int i, time;
 	OAM_Ent ent;
 	int offset, exit_code;
+	char LCDC;
 	VPPU3 *dut;
-
+	int tile_req;
 	offset = exit_code = 0;
 
-	for (i = 0; i < 160; i++)
-		OAM_BUFF[i] = 0; 
 
-	ent.y = 0;
-	ent.x = 30;
-	ent.tile_no = 5;
-	ent.flags = 5;
-
-	for (i = 0; i < 5; i++){}
-		OAM_BUFF[offset] = ent.y;
-		OAM_BUFF[offset+1] = ent.x;
-		OAM_BUFF[offset+2] = ent.tile_no;
-		OAM_BUFF[offset+3] = ent.flags;
-
-		offset += 4;
-	}
-	
 	Verilated::commandArgs(argc, argv);
 
 	dut = new VPPU3;  	// Instantiate the ppu module
@@ -47,15 +34,26 @@ int main(int argc, const char ** argv, const char ** env) {
 	dut->trace(tfp, 99);
 	tfp->open("ppu3.vcd");
 
-	for (time = 0 ; time < 10000 ; time += 10) {
-	    	dut->clk = ((time % 20) >= 10) ? 1 : 0;
-			dut->rst = 0;  	// Simulate a 50 MHz clock
-		if (time < 20) dut->rst = 1;
+	LCDC = 0xFF;
+	0x9C00
+	dut->WR = 1;
+	dut->ADDR = 0xFF40;
+	dut->MMIO_DATA_out = LCDC;
 
-    		dut->PPU_DATA_in = OAM_BUFF[dut->PPU_ADDR - 0xFE00];
-    		dut->eval();     			// Run the simulation for a cycle
-    		tfp->dump(time); 			// Write the VCD file for this cycle
-    	}
+	for (time = 0 ; time < 10000 ; time += 10) {
+    	dut->clk = ((time % 20) >= 10) ? 1 : 0; 	// Simulate a 50 MHz clock
+
+    	dut->rst = (time == 30) ? 1 : 0;	// pulses rst 
+		dut->PPU_DATA_in = 0;
+
+		//for map 2 and 8000 addressing method, due to LCDC = FF
+		tile_req = (dut->PPU_ADDR - 0x9C00)/16;
+		if (tile_req == 0) dut->PPU_DATA_in = 0;
+		if (tile_req == 1) dut->PPU_DATA_in = 1;
+
+    	dut->eval();     			// Run the simulation for a cycle
+    	tfp->dump(time); 			// Write the VCD file for this cycle
+    }
 
 	tfp->close(); // Stop dumping the VCD file
 	delete tfp;
