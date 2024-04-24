989a361edc23f7e74d251c4210812966e6dabf63
Donovan Sproule <donovan.sproule@yahoo.com>
2024-04-24 17:26:21 -0400

    sync2

--- GameBoy1/GameBoy_RTL_Qsys_submit/PPU3.sv
+++ GameBoy1/GameBoy_RTL_Qsys_submit/PPU3.sv
@@ -145,6 +145,43 @@ begin
     end
 end
 
+/* -- State Switching machine -- */
+always_ff @(posedge clk) begin
+    if (rst) begin
+			x_pos <= 0;
+			cycles <= 1;
+			LY <= 0;
+			PPU_ADDR <= `OAM_BASE_ADDR;
+			PPU_MODE <= SCAN;
+    end else if (LCDC[7]) begin
+    	cycles <= cycles + 1;
+		/* -- Following block happens on a per scanline basis (456 cycles per line) -- */
+        case (PPU_MODE)
+            SCAN: begin
+		    	if (PPU_ADDR > `OAM_END_ADDR) begin
+					PPU_MODE <= DRAW;
+					bg_fetch_mode <= TILE_NO_STORE;
+					x_pos <= 0;
+					PPU_ADDR <= `BG_MAP_1_BASE_ADDR;		// might shit the bed if we have 40 sprites
+		    	end
+				if (LY >= 144)
+					PPU_MODE <= V_BLANK;
+			end
+		    DRAW: 
+		    	if (x_pos > 144) PPU_MODE <= H_BLANK;
+		    H_BLANK: 
+		    	if (cycles >= 455) begin		// we reached the end of the scanline
+					LY <= LY + 1;
+					x_pos <= 0;
+					PPU_MODE <= SCAN;
+					cycles <= 0;
+				end
+		    V_BLANK: 							// not technically necessary but here for completeness
+				PPU_MODE <= H_BLANK;
+        endcase
+    end
+end   
+
 /* 
  * If we detect a memory request we return back the current
  * state of the register
@@ -229,9 +266,10 @@ always_ff @(posedge clk) begin
 					bg_fifo_load <= 1;
 					bg_fifo_go <= 0;
 					PPU_ADDR <= (`BG_MAP_1_BASE_ADDR + tile_c);
+					bg_fetch_mode <= TILE_NO_STORE;
 					tile_c <= tile_c + 1;
 					x_pos <= x_pos + 8;
-					pixels_pushed <= 7;
+					pixels_pushed <= 8;
 				end else begin 
 					PX_valid <= 1;
 					bg_fifo_go <= 1;
@@ -242,44 +280,6 @@ always_ff @(posedge clk) begin
 		endcase
 	end
 end
-   
-
-/* -- State Switching machine -- */
-always_ff @(posedge clk) begin
-    if (rst) begin
-			x_pos <= 0;
-			cycles <= 1;
-			LY <= 0;
-			PPU_ADDR <= `OAM_BASE_ADDR;
-			PPU_MODE <= SCAN;
-    end else if (LCDC[7]) begin
-    	cycles <= cycles + 1;
-		/* -- Following block happens on a per scanline basis (456 cycles per line) -- */
-        case (PPU_MODE)
-            SCAN: begin
-		    	if (PPU_ADDR >= `OAM_END_ADDR) begin
-					PPU_MODE <= DRAW;
-					bg_fetch_mode <= TILE_NO_STORE;
-					x_pos <= 0;
-					PPU_ADDR <= `BG_MAP_1_BASE_ADDR;		// might shit the bed if we have 40 sprites
-		    	end
-				if (LY >= 144)
-					PPU_MODE <= V_BLANK;
-			end
-		    DRAW: 
-		    	if (x_pos > 144) PPU_MODE <= H_BLANK;
-		    H_BLANK: 
-		    	if (cycles >= 455) begin		// we reached the end of the scanline
-					LY <= LY + 1;
-					x_pos <= 0;
-					PPU_MODE <= SCAN;
-					cycles <= 0;
-				end
-		    V_BLANK: 							// not technically necessary but here for completeness
-				PPU_MODE <= H_BLANK;
-        endcase
-    end
-end   
   
 endmodule
     
