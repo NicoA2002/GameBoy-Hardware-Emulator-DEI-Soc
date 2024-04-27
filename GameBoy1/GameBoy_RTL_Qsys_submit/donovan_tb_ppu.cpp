2371f9e973a8788b8fee3ccaef2a56f329d33b96
Donovan Sproule <donovan.sproule@yahoo.com>
2024-04-26 22:06:20 -0400

    debug

--- GameBoy1/GameBoy_RTL_Qsys_submit/donovan_tb_ppu.cpp
+++ GameBoy1/GameBoy_RTL_Qsys_submit/donovan_tb_ppu.cpp
@@ -51,7 +51,7 @@ int main(int argc, const char ** argv, const char ** env)
 			if (dut->PPU_ADDR >= BG_MAP_1_BASE_ADDR && dut->PPU_ADDR < BG_MAP_1_END_ADDR)
 				dut->PPU_DATA_in = (tile_c++ % 2) ? 0 : 1;
 
-			if (dut->PPU_ADDR >= TILE_BASE) 
+			if (dut->PPU_ADDR >= TILE_BASE && dut->clk == 1) 
 					dut->PPU_DATA_in = tile_2[row_code];
 					row_code = !row_code;
 		}
