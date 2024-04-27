5b2c6d38349bdbe929a5607819204dcc91d68e3e
Donovan Sproule <donovan.sproule@yahoo.com>
2024-04-26 22:09:02 -0400

    debug

--- GameBoy1/GameBoy_RTL_Qsys_submit/donovan_tb_ppu.cpp
+++ GameBoy1/GameBoy_RTL_Qsys_submit/donovan_tb_ppu.cpp
@@ -12,11 +12,11 @@ typedef enum {H_BLANK, V_BLANK, SCAN, DRAW} PPU_STATES_t;
 
 int main(int argc, const char ** argv, const char ** env) 
 {
-	int time, tile_c, offset, exit_code, row_code;
+	int time, tile_c, offset, exit_code, row_code, cycles;
 	char LCDC, tile_1[2], tile_2[2];
 	VPPU3 *dut;
 	
-	row_code = tile_c = offset = exit_code = 0;
+	cycles = row_code = tile_c = offset = exit_code = 0;
 
 	tile_1[0] = 0xFF;	// 1111_1111
 	tile_1[1] = 0x00;	// 0000_0000
@@ -41,17 +41,21 @@ int main(int argc, const char ** argv, const char ** env)
 	dut->ADDR = 0xFF40;
 	dut->MMIO_DATA_out = LCDC;
 
-	dut->PPU_DATA_in = 0;
 	for (time = 0 ; time < 10000 ; time += 10) {
     	dut->clk = ((time % 20) >= 10) ? 1 : 0; 	// Simulate a 50 MHz clock
+    	if (dut->clk == 1)
+    		cycles++;
 
     	dut->rst = (time == 30) ? 1 : 0;			// pulses rst 
+    	if (dut->rst == 1)
+    		cycles = 0;
+		dut->PPU_DATA_in = 0;
 		//for map 2 and 8000 addressing method, due to LCDC = FF
 		if (dut->PPU_MODE == DRAW) {
 			if (dut->PPU_ADDR >= BG_MAP_1_BASE_ADDR && dut->PPU_ADDR < BG_MAP_1_END_ADDR)
 				dut->PPU_DATA_in = (tile_c++ % 2) ? 0 : 1;
 
-			if (dut->PPU_ADDR >= TILE_BASE && dut->clk == 1) 
+			if (dut->PPU_ADDR >= TILE_BASE) 
 					dut->PPU_DATA_in = tile_2[row_code];
 					row_code = !row_code;
 		}
