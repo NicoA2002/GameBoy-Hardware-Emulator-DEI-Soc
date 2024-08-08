98752955f86a9deab0750688f2dd32510b446623
NicoA2002 <114366568+NicoA2002@users.noreply.github.com>
2024-08-07 20:50:28 -0400

    remove unncessary fiels

--- GameBoy_RTL_Qsys_submit/nicos_bad_tests.sh
+++
@@ -1,8 +0,0 @@
-cp PPU3.sv debug_tests/
-cp tb_ppu.cpp debug_tests/
-cd debug_tests/
-verilator --cc PPU3.sv
-verilator -trace -cc PPU3.sv -exe tb_ppu.cpp -top-module PPU3
-cd obj_dir && make -j -f VPPU3.mk
-./VPPU3
-gtkwave ppu3.vcd
